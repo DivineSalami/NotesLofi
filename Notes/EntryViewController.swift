5da72c332565f7657f56c2c19fe92125778d12bb
p.m.student <Bolden.Chance02@gmail.com>
2021-05-18 13:23:24 -0500

    final thing

---
+++ Notes/EntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryViewController.swift
+//  Notes
+//
+//  Created by P.M. Student on 4/22/21.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var noteField: UITextView!
+
+    public var completion: ((String, String) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.becomeFirstResponder()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
+    }
+
+    @objc func didTapSave() {
+        if let text = titleField.text, !text.isEmpty, !noteField.text.isEmpty {
+            completion?(text, noteField.text)
+        }
+    }
+
+
+}
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -5,15 +5,90 @@
 //  Created by P.M. Student on 4/14/21.
 //
 
+
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var AudioPlayer = AVAudioPlayer()
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var label: UILabel!
+
+    var models: [(title: String, note: String)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.delegate = self
+        table.dataSource = self
+        title = "Notes"
+        self.table.backgroundColor = UIColor.clear
+        
+        let AssortedMusics = NSURL(fileURLWithPath: Bundle.main.path(forResource: "milk2", ofType: "mp3")!)
+                AudioPlayer = try! AVAudioPlayer(contentsOf: AssortedMusics as URL)
+                AudioPlayer.prepareToPlay()
+                AudioPlayer.numberOfLoops = -1
+                AudioPlayer.play()
+
+           }
+
+           override func didReceiveMemoryWarning() {
+               super.didReceiveMemoryWarning()
+           }
+        
+    
+
+     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = UIColor.clear
+    }
+    
+
+    @IBAction func didTapNewNote() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "new") as? EntryViewController else {
+            return
+        }
+        vc.title = "New Note"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completion = { noteTitle, note in
+            self.navigationController?.popToRootViewController(animated: true)
+            self.models.append((title: noteTitle, note: note))
+            self.label.isHidden = true
+            self.table.isHidden = false
+
+            self.table.reloadData()
+        }
+        navigationController?.pushViewController(vc, animated: true)
     }
 
+    // Table
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].note
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let model = models[indexPath.row]
+
+        // Show note controller
+        guard let vc = storyboard?.instantiateViewController(identifier: "note") as? NoteViewController else {
+            return
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = "Note"
+        vc.noteTitle = model.title
+        vc.note = model.note
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
-
